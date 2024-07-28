c8f09d6a92bf99d7dc2df6fe95d8b43dacad57c5
Jardielson <jardielsonbio@gmail.com>
2024-07-27 22:14:33 -0300

    chore: adjustdatabase host on  docker-compose

--- billing/services/create_bill_service.py
+++ billing/services/create_bill_service.py
@@ -18,14 +18,13 @@ class CreateBillService:
                 'sended': False
             }
             bill = self.bill_model(**obj)
-        if not self.bill_model.objects.filter(
-            Q(government_id=bill.government_id) &
-            Q(email=bill.email) &
-            Q(debt_id=bill.debt_id) or
-            Q(sended=False)
-        ).exists():
-            to_create_bills.append(bill)
-
+            if not self.bill_model.objects.filter(
+                Q(government_id=bill.government_id) &
+                Q(email=bill.email) &
+                Q(debt_id=bill.debt_id) or
+                Q(sended=False)
+            ).exists():
+                to_create_bills.append(bill)
         if to_create_bills:
             self.bill_model.objects.bulk_create(to_create_bills)
         return to_create_bills
